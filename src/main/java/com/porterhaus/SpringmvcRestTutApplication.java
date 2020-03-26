3d2cc4e1d7a235dcc37098459d93810830dcc305
unknown <porterdbrian@gmail.com>
2020-03-26 16:25:32 -0500

    First commit

---
+++ src/main/java/com/porterhaus/SpringmvcRestTutApplication.java
@@ -0,0 +1,13 @@
+package com.porterhaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcRestTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcRestTutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porterhaus/SpringmvcRestTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porterhaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcRestTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
