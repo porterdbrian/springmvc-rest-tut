0470fdbe9be0fee44b76c15b8fc3980f9774590b
unknown <porterdbrian@gmail.com>
2020-03-28 19:56:14 -0500

    Security for User Signup 2 - not working

--- src/main/java/com/porterhaus/springmvcresttut/SpringmvcRestTutApplication.java
+++ src/main/java/com/porterhaus/springmvcresttut/SpringmvcRestTutApplication.java
@@ -2,6 +2,8 @@ package com.porterhaus.springmvcresttut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringmvcRestTutApplication {
@@ -9,5 +11,10 @@ public class SpringmvcRestTutApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcRestTutApplication.class, args);
 	}
-
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 }
